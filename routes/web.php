10573a7d3285e61a8f64086baf516a5be1c62e46
vagrant <vagrant@cocktales>
2016-12-14 13:45:52 +0000

    Auth initialised and user can now register and login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
